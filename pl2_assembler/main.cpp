bcc534182cc9f88a08619b0c40438fb83ddc8ddb
MassiveBattlebotsFan <teamlightning2018@outlook.com>
2021-08-08 14:28:11 -0500

    fixed opcode reading, and a couple other things

--- pl2_assembler/main.cpp
+++ pl2_assembler/main.cpp
@@ -12,7 +12,13 @@ using namespace std;
 
 int main(int argc, char const *argv[]) {
   try{
-    std::map<unsigned int, string> opcodes;
+    //init logging to file
+    ofstream logging("pl2asm.log");
+    //auto old_rdbuf = clog.rdbuf(); // not implemented
+    clog.rdbuf(logging.rdbuf());
+
+    std::map<uint8_t, string> opcodes = {};
+
     if(argc == 1){
       cerr << "Usage: " << argv[0] << " <source.pl2> [output.pl2c]" << endl;
       return -1;
@@ -24,30 +30,35 @@ int main(int argc, char const *argv[]) {
       outfile = infile + 'c';
     }
     ifstream opcodesIn;
-    opcodesIn.open("./opcodes.db");
-    if(opcodesIn.bad()){
-      throw("opcodes.db not found.");
+    opcodesIn.open("opcodes.db");
+    if(!opcodesIn.is_open()){
+      throw("opcodes.db open failed");
     }
     char buffer1[4096] = "", buffer2[4096] = "", buffer3[4096] = "";
+    stringstream parser;
     while(!opcodesIn.eof()){
+      parser.str("");
       opcodesIn.getline(buffer1, 4096, '\n');
       if(opcodesIn.fail()){
         if(opcodesIn.bad()){
-          throw("opcodes.db read stream broke");
+          throw("opcodes.db read stream bad");
         }
-        char temp[] = "opcodes.db read loop breaking, ";
-        throw(strcat(temp, strerror(errno)));
-        cerr << buffer1 << endl;
-        break;
+        if(opcodesIn.eof()){
+          break;
+        }
+        throw("opcodes.db read loop failed");
       }
-      stringstream parser;
-      parser.str(buffer1);
+      parser << buffer1 << '\0';
       parser.getline(buffer2, 4096, '=');
-      cerr << buffer2 << endl;
+      parser.getline(buffer3, 4096, '\0');
+      clog << buffer2 << ", " << buffer3 << endl;
+      opcodes.insert(std::pair<uint8_t, string>((uint8_t)strtol(buffer2, NULL, 16), (string)buffer3));
     }
+    opcodesIn.close();
+    
     return 0;
   }catch(const char* error){
-    cerr << "Error: " << error << endl;
+    cerr << "Error: " << error << ", errno = " << strerror(errno) << endl;
     return -1;
   }catch(...){
     cerr << "Unknown error. Program aborting." << endl;
