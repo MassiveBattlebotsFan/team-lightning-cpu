1d1e156756e0086558d814419c4c9f1aacffaf05
MassiveBattlebotsFan <teamlightning2018@outlook.com>
2021-08-09 16:50:45 -0500

    Changed opcodes.db, added IOCPU_opcodes.db, changed options

--- pl2_assembler/main.cpp
+++ pl2_assembler/main.cpp
@@ -20,17 +20,32 @@ int main(int argc, char const *argv[]) {
     std::map<string, uint16_t> opcodes = {};
     string opcodesDb = "opcodes.db";
     if(argc == 1){
-      cerr << "Usage: " << argv[0] << " <source.pl2> [opcodes.db]" << endl;
+      cerr << "Usage: " << argv[0] << " -i source.pl2 [-v] [-d opcodes.db] [-o output.pl2c]" << endl;
       return -1;
     }
-    string outfile, infile = argv[1];
-    //if(argc >= 3){
-    //  outfile = argv[2];
-    //}else{
-      outfile = infile + 'c';
-    //}
-    if(argc >= 2){
-      opcodesDb = argv[2];
+    for(int i = 1; i < argc; i++){
+      if(strcmp(argv[i], "-v")==0){
+        clog.rdbuf(old_rdbuf);
+        break;
+      }
+    }
+    string outfile, infile;
+    for(int i = 1; i < argc; i++){
+      if(strcmp(argv[i], "-i")==0){
+        infile = argv[i+1];
+        break;
+      }
+    }
+    outfile = infile + 'c';
+    for(int i = 1; i < argc; i++){
+      if(strcmp(argv[i], "-o")==0){
+        outfile = argv[i+1];
+        clog << "outfile set" << endl;
+      }
+      if(strcmp(argv[i], "-d")==0){
+        opcodesDb = argv[i+1];
+        clog << "db set" << endl;
+      }
     }
     ifstream opcodesIn;
     opcodesIn.open(opcodesDb);
@@ -39,6 +54,7 @@ int main(int argc, char const *argv[]) {
     }
     char buffer1[4096] = "", buffer2[4096] = "", buffer3[4096] = "";
     stringstream parser;
+    clog << "Opcodes read:" << endl;
     while(opcodesIn.getline(buffer1, 4096, '\n')){
       parser.str("");
       if(opcodesIn.fail()){
@@ -67,7 +83,7 @@ int main(int argc, char const *argv[]) {
     }
     uint16_t instrArgOut = 0x0;
     uint16_t opCodeOut = 0x0;
-
+    clog << "Assembler output:" << endl;
     while(asmIn.getline(buffer1, 4096, '\n')){
       parser.str("");
       if(asmIn.fail()){
